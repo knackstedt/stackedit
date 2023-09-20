2f871bee446c457730ff2217be4ceebfd5c1dcc9
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-09-20 10:33:20 -0400

    🐛fix(mermaid-layouts) cleanup examples, fix formatting issues

--- src/app/editor/components/toolbar/mermaid-layouts.ts
+++ src/app/editor/components/toolbar/mermaid-layouts.ts
@@ -1,8 +1,7 @@
 export default [{
             label: "Flow Chart", value: `\n\`\`\`mermaid
 flowchart LR
-    markdown[This ** is ** _Markdown_
-            ]
+    markdown[This ** is ** _Markdown_]
     newLines["Line1
     Line 2
     Line 3"]
@@ -12,59 +11,64 @@ flowchart LR
             {
             label: "Sequence Diagram", value: `\n\`\`\`mermaid
 sequenceDiagram
-Alice->>John: Hello John, how are you?
-John-->>Alice: Great!
-Alice-)John: See you later!
+    Alice ->> Bob: Hello Bob, how are you?
+    Bob-->>John: How about you John?
+    Bob--x Alice: I am good thanks!
+    Bob-x John: I am good thanks!
+    Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
+
+    Bob-->Alice: Checking with John...
+    Alice->John: Yes... John, how are you?
 \`\`\``
             },
             {
             label: "Class Diagram", value: `\n\`\`\`mermaid
 classDiagram
+    note "From Duck till Zebra"
     Animal <|-- Duck
+    note for Duck "can fly\ncan swim\ncan dive\ncan help in debugging"
     Animal <|-- Fish
     Animal <|-- Zebra
     Animal : +int age
     Animal : +String gender
     Animal: +isMammal()
     Animal: +mate()
-
     class Duck{
         +String beakColor
         +swim()
         +quack()
-                }
+    }
     class Fish{
         -int sizeInFeet
         -canEat()
-                }
+    }
     class Zebra{
         +bool is_wild
         +run()
-                }
+    }
 \`\`\``
             },
             {
             label: "State Diagram", value: `\n\`\`\`mermaid
 stateDiagram-v2
-    [*
-                ] --> Still
-    Still --> [*
-                ]
+    [*] --> Still
+    Still --> [*]
 
     Still --> Moving
     Moving --> Still
     Moving --> Crash
-    Crash --> [*
-                ]
+    Crash --> [*]
 \`\`\``
             },
             {
             label: "Entity Relationship Diagram", value: `\n\`\`\`mermaid
+---
+title: Order example
+---
 erDiagram
     CUSTOMER ||--o{ ORDER : places
     ORDER ||--|{ LINE-ITEM : contains
-    CUSTOMER
-                    }|..|{ DELIVERY-ADDRESS : uses
+    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses
 \`\`\``
                     },
                     {
@@ -72,29 +76,25 @@ erDiagram
 journey
     title My working day
     section Go to work
-    Make tea: 5: Me
-    Go upstairs: 3: Me
-    Do work: 1: Me, Cat
+      Make tea: 5: Me
+      Go upstairs: 3: Me
+      Do work: 1: Me, Cat
     section Go home
-    Go downstairs: 5: Me
-    Sit down: 5: Me
+      Go downstairs: 5: Me
+      Sit down: 5: Me
 \`\`\``
                     },
                     {
             label: "Gantt Chart", value: `\n\`\`\`mermaid
 gantt
     title A Gantt Diagram
-    dateFormat  YYYY-MM-DD
+    dateFormat YYYY-MM-DD
     section Section
-    A task           :a1,
-                        2014-01-01,
-                        30d
-    Another task     :after a1  ,
-                        20d
+        A task          :a1, 2014-01-01, 30d
+        Another task    :after a1, 20d
     section Another
-    Task in sec      : 2014-01-12,
-                        12d
-    another task      : 24d
+        Task in Another :2014-01-12, 12d
+        another task    :24d
 \`\`\``
                     },
                     {
@@ -110,15 +110,15 @@ pie title Pets adopted by volunteers
 requirementDiagram
 
     requirement test_req {
-    id: 1
-    text: the test text.
-    risk: high
-    verifymethod: test
-                        }
+        id: 1
+        text: the test text.
+        risk: high
+        verifymethod: test
+    }
 
     element test_entity {
-    type: simulation
-                        }
+        type: simulation
+    }
 
     test_entity - satisfies -> test_req
 \`\`\``
@@ -126,10 +126,22 @@ requirementDiagram
                     {
             label: "Mindmap", value: `\n\`\`\`mermaid
 mindmap
-    Root
-        A
-          B
-          C
+  root((mindmap))
+    Origins
+      Long history
+      ::icon(fa fa-book)
+      Popularisation
+        British popular psychology author Tony Buzan
+    Research
+      On effectiveness<br/>and features
+      On Automatic creation
+        Uses
+            Creative techniques
+            Strategic planning
+            Argument mapping
+    Tools
+      Pen and paper
+      Mermaid
 \`\`\``
                     },
                     {
@@ -138,7 +150,7 @@ timeline
     title History of Social Media Platform
     2002 : LinkedIn
     2004 : Facebook
-        : Google
+         : Google
     2005 : Youtube
     2006 : Twitter
 \`\`\``
