d8507f7971f822bda46b8b3b95a1071d62c651d0
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-09-22 09:31:13 -0400

    🐛fix(markdown-renderer) render task lists with formatting in the item

--- src/app/editor/extensions/libs/markdownItTasklist.ts
+++ src/app/editor/extensions/libs/markdownItTasklist.ts
@@ -1,40 +1,124 @@
-function attrSet(token, name, value) {
-  const index = token.attrIndex(name);
-  const attr = [name, value];
+import MarkdownIt, { Token } from 'markdown-it';
 
-  if (index < 0) {
-    token.attrPush(attr);
-  } else {
-    token.attrs[index] = attr;
-  }
+/**
+ * Original: https://github.com/revin/markdown-it-task-lists/blob/master/index.js (Sep 22, 2023)
+ * (ISC License)
+ *
+ */
+let disableCheckboxes = true;
+let useLabelWrapper = false;
+let useLabelAfter = false;
+
+const render: MarkdownIt.PluginWithOptions = (md, options) => {
+    if (options) {
+        disableCheckboxes = !options.enabled;
+        useLabelWrapper = !!options.label;
+        useLabelAfter = !!options.labelAfter;
+    }
+
+    md.core.ruler.after('inline', 'github-task-lists', function (state) {
+        const tokens = state.tokens;
+        for (let i = 2; i < tokens.length; i++) {
+            if (isTodoItem(tokens, i)) {
+                todoify(tokens[i], state.Token);
+                attrSet(tokens[i - 2], 'class', 'task-list-item' + (!disableCheckboxes ? ' enabled' : ''));
+                attrSet(tokens[parentToken(tokens, i - 2)], 'class', 'contains-task-list');
+            }
+        }
+    });
+};
+export default render;
+
+
+function attrSet(token, name, value) {
+    const index = token.attrIndex(name);
+    const attr = [name, value];
+
+    if (index < 0) {
+        token.attrPush(attr);
+    }
+    else {
+        token.attrs[index] = attr;
+    }
 }
 
-export default (md) => {
-  md.core.ruler.after('inline', 'tasklist', ({ tokens, Token }) => {
-    for (let i = 2; i < tokens.length; i += 1) {
-      const token = tokens[i];
-      if (token.content
-        && token.content.charCodeAt(0) === 0x5b /* [ */
-        && token.content.charCodeAt(2) === 0x5d /* ] */
-        && token.content.charCodeAt(3) === 0x20 /* space */
-        && token.type === 'inline'
-        && tokens[i - 1].type === 'paragraph_open'
-        && tokens[i - 2].type === 'list_item_open'
-      ) {
-        const cross = token.content[1].toLowerCase();
-        if (cross === ' ' || cross === 'x') {
-          const checkbox = new Token('html_inline', '', 0);
-          if (cross === ' ') {
-            checkbox.content = '<span class="task-list-item-checkbox" type="checkbox">&#9744;</span>';
-          } else {
-            checkbox.content = '<span class="task-list-item-checkbox checked" type="checkbox">&#9745;</span>';
-          }
-          token.children.unshift(checkbox);
-          token.children[1].content = token.children[1].content.slice(3);
-          token.content = token.content.slice(3);
-          attrSet(tokens[i - 2], 'class', 'task-list-item');
+function parentToken(tokens, index: number) {
+    const targetLevel = tokens[index].level - 1;
+    for (let i = index - 1; i >= 0; i--) {
+        if (tokens[i].level === targetLevel) {
+            return i;
         }
-      }
     }
-  });
-};
+    return -1;
+}
+
+function isTodoItem(tokens, index: number) {
+    return isInline(tokens[index]) &&
+        isParagraph(tokens[index - 1]) &&
+        isListItem(tokens[index - 2]) &&
+        startsWithTodoMarkdown(tokens[index]);
+}
+
+function todoify(token, TokenConstructor: Token) {
+    token.children.unshift(makeCheckbox(token, TokenConstructor));
+    token.children[1].content = token.children[1].content.slice(3);
+    token.content = token.content.slice(3);
+
+    if (useLabelWrapper) {
+        if (useLabelAfter) {
+            token.children.pop();
+
+            // Use large random number as id property of the checkbox.
+            const id = 'task-item-' + Math.ceil(Math.random() * (10000 * 1000) - 1000);
+            token.children[0].content = token.children[0].content.slice(0, -1) + ' id="' + id + '">';
+            token.children.push(afterLabel(token.content, id, TokenConstructor));
+        }
+        else {
+            token.children.unshift(beginLabel(TokenConstructor));
+            token.children.push(endLabel(TokenConstructor));
+        }
+    }
+}
+
+function makeCheckbox(token, TokenConstructor: Token) {
+    const checkbox = new TokenConstructor('html_inline', '', 0);
+    const disabledAttr = disableCheckboxes ? ' disabled="" ' : '';
+
+    if (token.content.indexOf('[ ] ') === 0) {
+        checkbox.content = '<input class="task-list-item-checkbox"' + disabledAttr + 'type="checkbox">';
+    }
+    else if (token.content.indexOf('[x] ') === 0 || token.content.indexOf('[X] ') === 0) {
+        checkbox.content = '<input class="task-list-item-checkbox" checked=""' + disabledAttr + 'type="checkbox">';
+    }
+    return checkbox;
+}
+
+// these next two functions are kind of hacky; probably should really be a
+// true block-level token with .tag=='label'
+function beginLabel(TokenConstructor: Token) {
+    const token = new TokenConstructor('html_inline', '', 0);
+    token.content = '<label>';
+    return token;
+}
+
+function endLabel(TokenConstructor: Token) {
+    const token = new TokenConstructor('html_inline', '', 0);
+    token.content = '</label>';
+    return token;
+}
+
+function afterLabel(content: string, id: string, TokenConstructor: Token) {
+    const token = new TokenConstructor('html_inline', '', 0);
+    token.content = '<label class="task-list-item-label" for="' + id + '">' + content + '</label>';
+    token.attrs = [{ for: id }] as any;
+    return token;
+}
+
+function isInline(token) { return token.type === 'inline'; }
+function isParagraph(token) { return token.type === 'paragraph_open'; }
+function isListItem(token) { return token.type === 'list_item_open'; }
+
+function startsWithTodoMarkdown(token) {
+    // leading whitespace in a list item is already trimmed off by markdown-it
+    return token.content.indexOf('[ ] ') === 0 || token.content.indexOf('[x] ') === 0 || token.content.indexOf('[X] ') === 0;
+}
