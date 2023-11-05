79a56b7def4c98f67ce7a49d1efdcd64354f3000
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-11-05 10:05:16 -0500

    ⚙chore(services) add fetch service and piston bindings

---
+++ src/app/services/fetch.service.ts
@@ -0,0 +1,129 @@
+import { HttpClient, HttpContext, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Injectable, isDevMode } from '@angular/core';
+import type http from '@tauri-apps/api/http';
+import { retry } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { ResponseType } from '@tauri-apps/api/http';
+
+const { fetch }
+    = window['__TAURI__']?.['http'] as typeof http || {};
+
+
+// Total number of _retries_ if there is a 429 response code.
+const retryCount = 2;
+
+export type FetchOptions = {
+    headers?: HttpHeaders | {
+        [header: string]: string | string[];
+    };
+    context?: HttpContext;
+    params?: HttpParams | {
+        [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
+    };
+    body?: any,
+    observe?: 'body' | 'events' | 'response';
+    reportProgress?: boolean;
+    responseType?: 'arraybuffer' | 'blob' | 'json' | 'text';
+    withCredentials?: boolean;
+};
+
+@Injectable({
+    providedIn: "root"
+})
+export class Fetch {
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    // Public interface for making AJAX transactions
+    public get<T>(url: string, options: FetchOptions = {}, returnError = false): Promise<T> {
+        return this.request<T>("get", url, options, returnError);
+    }
+    public put<T>(url: string, body: any, options: FetchOptions = {}, returnError = false): Promise<T> {
+        options.body = (options.body && Object.keys(options.body).length > 0 ? options.body : body) || {};
+        return this.request<T>("put", url, options, returnError);
+    }
+    public post<T>(url: string, body: any, options: FetchOptions = {}, returnError = false): Promise<T> {
+        options.body = (options.body && Object.keys(options.body).length > 0 ? options.body : body) || {};
+        return this.request<T>("post", url, options, returnError);
+    }
+    public patch<T>(url: string, body: any, options: FetchOptions = {}, returnError = false): Promise<T> {
+        options.body = (options.body && Object.keys(options.body).length > 0 ? options.body : body) || {};
+        return this.request<T>("patch", url, options, returnError);
+    }
+    public delete<T>(url: string, options: FetchOptions = {}, returnError = false): Promise<T> {
+        return this.request<T>("delete", url, options, returnError);
+    }
+
+    // Internally, handle the observable as a promise.
+    private async request<T>(method: string, url: string, options: FetchOptions = {}, returnDetails = false): Promise<T> {
+        options.reportProgress = true;
+
+        // Allow support for different response types.
+        // Generally we shouldn't need this to be anything other than JSON.
+        options.responseType = options.responseType || "json";
+        options.observe = "response";
+        options.withCredentials = true;
+
+        if (fetch) {
+            const res = await fetch<any>(url, {
+                method: method.toUpperCase() as any,
+                body: options.body,
+                headers: options.headers,
+                responseType: ResponseType.Binary,
+            });
+
+            let data;
+            let originalData = res.data;
+            window['__data'] = originalData;
+            const contentType = res.headers['content-type'] as string;
+            switch(true) {
+                case (contentType.startsWith("application/json")): {
+                    let text = res.data.map(code => String.fromCharCode(code)).join('');
+                    data = res.data = JSON.parse(text);
+                    break;
+                }
+                // Media types all return as blobs
+                case (/image\//.test(contentType)): {
+                    data = res.data = new Blob(res.data);
+                    break;
+                }
+                default: {
+                    let text = res.data.map(code => String.fromCharCode(code)).join('');
+                    data = res.data = text;
+                }
+            }
+
+            return returnDetails ? res : data as any;
+        }
+        else {
+
+            const p = new Promise((resolve, reject) => {
+                const o = this.http.request(method, url, options)
+                    .pipe(retry({
+                        delay(error, retryCount) {
+                            // 429 and 502 are most common for overloaded
+                            // backends -- so we'll retry if we get these errors
+                            if (error.status == 429 || error.status == 502)
+                                return of({});
+
+                            if (error.status == 504 && isDevMode())
+                                alert("It looks like you can't reach your development backend anymore");
+
+                            throw error;
+                        },
+                        count: retryCount
+                    }))
+                    .subscribe(data => {
+                        resolve(returnDetails ? data : data.data as any);
+
+                        // provide 3ms slacktime before releasing observable.
+                        setTimeout(() => {
+                            o.unsubscribe();
+                        }, 3);
+                    });
+            });
+            return p as Promise<T>;
+        }
+    }
+}
--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -3,16 +3,36 @@
 import { Injectable } from '@angular/core';
 import { ConfirmDialogComponent } from '../components/@dialogs/confirm-dialog/confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Fetch } from './fetch.service';
+
+type PistonLanguage = { language: string, version: string, aliases: string[]; }
+type PistonFile = { name: string, content: string, encoding: string };
+type PistonResponse = {
+    stdout: string,
+    stderr: string,
+    output: string,
+    code: number,
+    signal: unknown
+}
+type PistonExecResult = {
+    language: string,
+    version: string,
+    run?: PistonResponse
+    compile?: PistonResponse
+};
 
 @Injectable({
     providedIn: 'root'
 })
 export class UtilService {
 
-    constructor(
-        private readonly dialog: MatDialog
-    ) {
+    pistonRuntimes: PistonLanguage[] = [];
 
+    constructor(
+        private readonly dialog: MatDialog,
+        private readonly fetch: Fetch
+    ) {
+        this.getPistonRuntimes();
     }
 
     confirmAction(title: string, message: string) {
@@ -29,5 +49,21 @@ export class UtilService {
                     s.unsubscribe();
                 });
         });
+    }
+
+    async getPistonRuntimes() {
+        console.log("what the actual hell")
+        this.pistonRuntimes = this.pistonRuntimes?.length
+            ? this.pistonRuntimes
+            : await this.fetch.get<PistonLanguage[]>("https://emkc.org/api/v2/piston/runtimes");
+        console.log("promise me ", this.pistonRuntimes)
+        return this.pistonRuntimes;
+    }
+
+    async runPistonScript(lang: PistonLanguage, files: PistonFile[]): Promise<PistonExecResult> {
+        return this.fetch.post("https://emkc.org/api/v2/piston/execute", {
+            ...lang,
+            files
+        })
     }
 }
