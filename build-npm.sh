fdf4783d27db1cb7515d2f857aea51cb013fa0a1
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-12-30 17:36:31 -0500

    ⚙chore(build) disable version backset

--- build-npm.sh
+++ build-npm.sh
@@ -16,21 +16,21 @@ git commit -m "⚛ Bump version"
 
 echo "extracted version: $version"
 
-npm i \
-    @angular/animations@15 \
-    @angular/cdk@15 \
-    @angular/common@15 \
-    @angular/compiler@15 \
-    @angular/core@15 \
-    @angular/forms@15 \
-    @angular/material@15 \
-    @angular/platform-browser@15 \
-    @angular/platform-browser-dynamic@15 \
-    @angular/service-worker@15 \
-    @angular-devkit/build-angular@15 \
-    @angular/cli@15 \
-    @angular/compiler-cli@15 \
-    --force
+# npm i \
+#     @angular/animations@15 \
+#     @angular/cdk@15 \
+#     @angular/common@15 \
+#     @angular/compiler@15 \
+#     @angular/core@15 \
+#     @angular/forms@15 \
+#     @angular/material@15 \
+#     @angular/platform-browser@15 \
+#     @angular/platform-browser-dynamic@15 \
+#     @angular/service-worker@15 \
+#     @angular-devkit/build-angular@15 \
+#     @angular/cli@15 \
+#     @angular/compiler-cli@15 \
+#     --force
 
 npm run build
 
